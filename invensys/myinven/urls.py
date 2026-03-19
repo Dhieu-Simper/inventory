8bc3228e4e869cbc0625d1b24c3c5d6da3d5341e
Dhieu-Simper <167874706+Dhieu-Simper@users.noreply.github.com>
2026-03-19 14:42:01 +0300

    Update urls.py

--- invensys/myinven/urls.py
+++ invensys/myinven/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     # Dashboard
+    
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
     path('', views.dashboard, name='dashboard'),
 
     # Categories
@@ -22,4 +25,12 @@ urlpatterns = [
     path('products/add/', views.product_create, name='product_create'),
     path('products/edit/<int:pk>/', views.product_update, name='product_update'),
     path('products/delete/<int:pk>/', views.product_delete, name='product_delete'),
+
+    # Purchases
+    path('purchases/', views.purchase_list, name='purchase_list'),
+    path('purchases/add/', views.purchase_create, name='purchase_create'),
+
+    # Sales
+    path('sales/', views.sale_list, name='sale_list'),
+    path('sales/add/', views.sale_create, name='sale_create'),
 ]
